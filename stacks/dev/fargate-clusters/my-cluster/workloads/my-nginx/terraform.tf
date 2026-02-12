2b9fc42c6d4041244bad4cc16273dd36a1fd3ec6
Soren Martius <soeren.martius@gmail.com>
2026-02-12 00:11:04 -0300

    fix: generate missing providers for ecs service

--- stacks/dev/fargate-clusters/my-cluster/workloads/my-nginx/terraform.tf
+++ stacks/dev/fargate-clusters/my-cluster/workloads/my-nginx/terraform.tf
@@ -3,3 +3,18 @@
 terraform {
   required_version = "1.14.1"
 }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.25.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
